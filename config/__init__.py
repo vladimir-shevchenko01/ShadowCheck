8dbe88d199f12fb10355e21f2dcacaf71a4f44c4
Vladimir <prstud@yandex.ru>
2026-03-02 19:12:23 +0300

    импорты модуля конфиг -> __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,8 @@
-# config/__init__.py
 """
 Модуль конфигурации проекта.
 Предоставляет глобальный объект config с валидированными настройками.
 """
+
 from .loader import config
 from .logger_config import get_logger, setup_logging
 from .models import AppConfig
